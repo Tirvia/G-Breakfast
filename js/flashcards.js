dd6a36e88c0cc08f9b0cc3d05d90f5baa5fc5692
Hanna <112614483+Tirvia@users.noreply.github.com>
2026-03-05 16:04:08 +0300

    Update flashcards.js

--- js/flashcards.js
+++ js/flashcards.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
    var cards = [
-        {id:0, question:'./images/Английский завтрак.png', answer:'./images/Английский завтрак1.png'},
-        {id:1, question:'./images/Беларуский завтрак.png', answer:'./images/Беларуский завтрак1.png'},
-        {id:2, question:'./images/Блинчики с творогом. png.png', answer:'./images/Блинчики с творогом1. png.png'},
-        {id:3, question:'./images/Беларуский завтрак.png', answer:'./images/Беларуский завтрак1.png'},
-        {id:4, question:'./images/большой блин.png', answer:'./images/большой блин1.png'},
-        {id:5, question:'./images/Бульон с филе индейки.png', answer:'./images/Бульон с филе индейки1.png'},
-        {id:6, question:'./images/Гречневая каша.png', answer:'./images/Гречневая каша1.png'},
-        {id:7, question:'./images/Йогурт с чиа.png', answer:'./images/Йогурт с чиа1.png'},
-        {id:8, question:'./images/Мексиканский завтрак.png', answer:'./images/Мексиканский завтрак1.png'},
-        {id:9, question:'./images/Немецкий завтрак.png', answer:'./images/Немецкий завтрак1.png'},
-        {id:10, question:'./images/Овсянка.png', answer:'./images/Овсянка1.png'},
-        {id:11, question:'./images/Омлет с салатом.png', answer:'./images/Омлет с салатом1.png'},
-        {id:12, question:'./images/Омлет с ссалатом и беконом.png', answer:'./images/Омлет с ссалатом и беконом1.png'},
-        {id:13, question:'./images/Омлет с ссалатом и курицей.png', answer:'./images/Омлет с ссалатом и курицей1.png'},
-        {id:14, question:'./images/Омлет с ссалатом и лососем.png', answer:'./images/Омлет с ссалатом и лососем1.png'},
-        {id:15, question:'./images/Сырная овсянка.png', answer:'./images/Сырная овсянка1.png'},
-        {id:16, question:'./images/Яйца Бенедикт с беконом.png', answer:'./images/Яйца Бенедикт с беконом1.png'},
-        {id:17, question:'./images/Яйца Бенедикт с лососем.png', answer:'./images/Яйца Бенедикт с лососем1.png'},
-        {id:18, question:'./images/Яйца Бенедикт с цыпленком.png', answer:'./images/Яйца Бенедикт с цыпленком1.png'}
+        {id:0, question:'./images/1.png', answer:'./images/2.png'},
+        {id:1, question:'./images/3.png', answer:'./images/4.png'},
+        {id:2, question:'./images/5.png', answer:'./images/6.png'},
+        {id:3, question:'./images/7.png', answer:'./images/8.png'},
+        {id:4, question:'./images/9.png', answer:'./images/10.png'},
+        {id:5, question:'./images/11.png', answer:'./images/12.png'},
+        {id:6, question:'./images/13.png', answer:'./images/14.png'},
+        {id:7, question:'./images/15.png', answer:'./images/16.png'},
+        {id:8, question:'./images/17.png', answer:'./images/18.png'},
+        {id:9, question:'./images/19.png', answer:'./images/20.png'},
+        {id:10, question:'./images/21.png', answer:'./images/22.png'},
+        {id:11, question:'./images/23.png', answer:'./images/24.png'},
+        {id:12, question:'./images/25.png', answer:'./images/26.png'},
+        {id:13, question:'./images/27.png', answer:'./images/28.png'},
+        {id:14, question:'./images/29.png', answer:'./images/30.png'}
     ];
 
-   
     var currentCardIndex;
     var isShowingAnswer = false;
     
